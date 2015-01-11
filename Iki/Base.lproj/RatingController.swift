df7fc8abc1733046876dbc2e44ef22bd1d7f0600
jonathanpeterwu <jpwu03@yahoo.com>
2015-01-10 17:21:54 -0800

    update slides

---
+++ Iki/Base.lproj/RatingController.swift
@@ -0,0 +1,26 @@
+//
+//  RatingController.swift
+//  
+//
+//  Created by Johnny Wu on 1/10/15.
+//
+//
+
+import Foundation
+import UIKit
+
+class RatingController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
